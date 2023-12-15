cc8e8dbbdffdeb9ee039df1cd8722d32435d8d54
elops-od <elops-od@git.com>
2023-12-15 12:06:01 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,120 @@
-# echo-server
+## Sample gRPC Requests
+
+* Reflection
+  ```shell
+  $ grpcurl -v -plaintext  -proto ./pkg/greeter/greeter.proto localhost:80 list
+  ```
+  ```text
+  EchoServer
+  grpc.reflection.v1.ServerReflection
+  grpc.reflection.v1alpha.ServerReflection
+  ```
+
+* GRPC request
+  ```shell
+  grpcurl -d '{"name": "El"}' --plaintext 127.0.0.1:50051  EchoServer/GrpcPing
+  ```
+  ```text
+  {
+  "message": "Hello, El!",
+  "headers": "map[:authority:[127.0.0.1:50053] content-type:[application/grpc] grpc-accept-encoding:[gzip] user-agent:[grpcurl/v1.8.9 grpc-go/1.57.0]]"
+  }
+  ```
+  * GRPCs request
+  ```shell
+  grpcurl -d '{"name": "El"}' --insecure 127.0.0.1:50053   EchoServer/GrpcPing
+  ```
+  ```text
+  {
+  "message": "Hello, El!",
+  "headers": "map[:authority:[127.0.0.1:50053] content-type:[application/grpc] grpc-accept-encoding:[gzip] user-agent:[grpcurl/v1.8.9 grpc-go/1.57.0]]"
+  }
+  ```
+
+## Sample REST Requests
+
+* Using HTTP/1.1 
+
+  ```shell
+  $ curl -v --raw -d '{"Message":"Hello, El!"}' --http1.1 http://localhost:8080
+  ```
+  
+  ```text
+  > POST / HTTP/1.1
+  > Host: localhost:8080
+  > User-Agent: curl/7.81.0
+  > Accept: */*
+  > Content-Length: 24
+  > Content-Type: application/x-www-form-urlencoded
+  >
+  * Mark bundle as not supporting multiuse
+    < HTTP/1.1 200 OK
+    < Date: Fri, 15 Dec 2023 09:49:42 GMT
+    < Content-Length: 240
+    < Content-Type: text/plain; charset=utf-8
+    <
+    Host: localhost:8080
+    Path: /
+    Method: POST
+    Protocol: HTTP/1.1
+    Scheme: http
+    Hostname: pc
+    Headers:
+      Content-Length: 24
+      Content-Type: application/x-www-form-urlencoded
+      User-Agent: curl/7.81.0
+      Accept: */*
+    Body:
+    {"Message":"Hello, El!"}
+  ```
+
+* Using HTTP/2
+
+  ```shell
+  $ curl -vk --raw -d '{"Message":"Hello, El!"}' --http2 https://localhost:8443
+  ```
+    
+  ```text
+  > POST / HTTP/2
+  > Host: localhost:8443
+  > user-agent: curl/7.81.0
+  > accept: */*
+  > content-length: 24
+  > content-type: application/x-www-form-urlencoded
+  >
+  * TLSv1.2 (IN), TLS header, Supplemental data (23):
+    * TLSv1.3 (IN), TLS handshake, Newsession Ticket (4):
+    * TLSv1.2 (IN), TLS header, Supplemental data (23):
+    * Connection state changed (MAX_CONCURRENT_STREAMS == 250)!
+    * TLSv1.2 (OUT), TLS header, Supplemental data (23):
+    * TLSv1.2 (OUT), TLS header, Supplemental data (23):
+    * We are completely uploaded and fine
+    * TLSv1.2 (IN), TLS header, Supplemental data (23):
+    * TLSv1.2 (IN), TLS header, Supplemental data (23):
+      < HTTP/2 200
+      < content-type: text/plain; charset=utf-8
+      < content-length: 241
+      < date: Fri, 15 Dec 2023 09:50:56 GMT
+      <
+      Host: localhost:8443
+      Path: /
+      Method: POST
+      Protocol: HTTP/2.0
+      Scheme: https
+      Hostname: pc
+      Headers:
+        Content-Length: 24
+        Content-Type: application/x-www-form-urlencoded
+        User-Agent: curl/7.81.0
+        Accept: */*
+      Body:
+      {"Message":"Hello, El!"}
+  ```
+  
+## Proto
+* Generate proto
+  ```shell
+  go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+  go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+  protoc *.proto --go_out=./ --go-grpc_out=./
+  ```
---
+++ main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"github.com/caitlinelfring/go-env-default"
+	pb "github.com/elopsod/echo-server/echoServer"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/reflection"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+)
+
+func main() {
+
+	httpPort := env.GetDefault("HTTP_PORT", "8080")
+	httpsPort := env.GetDefault("HTTPS_PORT", "8443")
+	grpcPort := env.GetDefault("GRPC_PORT", "50051")
+	grpcsPort := env.GetDefault("GRPCS_PORT", "50053")
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// HTTP server setup
+	go func() {
+		defer wg.Done()
+
+		httpHandler := http.NewServeMux()
+		httpHandler.HandleFunc("/", HttpPing)
+
+		httpServer := &http.Server{
+			Addr:    fmt.Sprintf(":%s", httpPort),
+			Handler: httpHandler,
+		}
+		log.Printf("HTTP Server is listening on port %s", httpPort)
+		if err := httpServer.ListenAndServe(); err != nil {
+			log.Fatalf("failed to serve HTTP: %v", err)
+		}
+	}()
+
+	// HTTPS server setup
+	go func() {
+		defer wg.Done()
+
+		httpsHandler := http.NewServeMux()
+		httpsHandler.HandleFunc("/", HttpPing)
+
+		certFile := "certs/server.crt"
+		keyFile := "certs/server.key"
+
+		certificate, _ := tls.LoadX509KeyPair(certFile, keyFile)
+		tlsConfig := &tls.Config{
+			Certificates: []tls.Certificate{certificate},
+		}
+
+		httpsServer := &http.Server{
+			Addr:      fmt.Sprintf(":%s", httpsPort),
+			Handler:   httpsHandler,
+			TLSConfig: tlsConfig,
+		}
+
+		http.HandleFunc("/", HttpPing)
+		log.Printf("HTTPS Server is listening on port %s", httpsPort)
+		if err := httpsServer.ListenAndServeTLS(certFile, keyFile); err != nil {
+			log.Fatalf("failed to serve HTTPS: %v", err)
+		}
+	}()
+	// GRPC server setup
+	go func() {
+		defer wg.Done()
+		listener, err := net.Listen("tcp", fmt.Sprintf(":%s", grpcPort))
+		if err != nil {
+
+			log.Fatalf("Failed to listen: %v", err)
+		}
+
+		srv := grpc.NewServer()
+		pb.RegisterEchoServerServer(srv, &server{})
+
+		reflection.Register(srv)
+
+		log.Printf("GRPC Server is listening on port %s", grpcPort)
+		if err := srv.Serve(listener); err != nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+	}()
+
+	// GRPCS server setup
+	go func() {
+		defer wg.Done()
+		certFile := "certs/server.crt"
+		keyFile := "certs/server.key"
+		serverCert, _ := tls.LoadX509KeyPair(certFile, keyFile)
+
+		creds := credentials.NewTLS(&tls.Config{
+			Certificates: []tls.Certificate{serverCert},
+		})
+
+		srv := grpc.NewServer(grpc.Creds(creds))
+
+		pb.RegisterEchoServerServer(srv, &server{})
+		reflection.Register(srv)
+
+		listener, _ := net.Listen("tcp", fmt.Sprintf(":%s", grpcsPort))
+
+		log.Printf("GRPCS Server is listening on port %s", grpcsPort)
+		if err := srv.Serve(listener); err != nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+	}()
+
+	wg.Wait()
+}
+
+func HttpPing(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	host := r.Host
+	method := r.Method
+	protocol := r.Proto
+	hostname, _ := os.Hostname()
+	var scheme string = "http"
+	if r.TLS != nil {
+		scheme = "https"
+	}
+	body, _ := io.ReadAll(r.Body)
+
+	headers := []string{}
+	for key, values := range r.Header {
+		headers = append(headers, fmt.Sprintf("%s: %s", key, strings.Join(values, " ")))
+	}
+	// Send headers, path, and body in the response
+	fmt.Fprintln(w, "Host:", host)
+	fmt.Fprintln(w, "Path:", path)
+	fmt.Fprintln(w, "Method:", method)
+	fmt.Fprintln(w, "Protocol:", protocol)
+	fmt.Fprintln(w, "Scheme:", scheme)
+	fmt.Fprintln(w, "Hostname:", hostname)
+	fmt.Fprintln(w, "Headers:\n\t", strings.Join(headers[:], "\n\t "))
+	fmt.Fprintln(w, "Body:\n\t", string(body))
+}
+
+type server struct {
+	pb.EchoServerServer
+}
+
+func (s *server) GrpcPing(ctx context.Context, req *pb.Request) (*pb.Response, error) {
+	name := req.GetName()
+	md, _ := metadata.FromIncomingContext(ctx)
+
+	response := &pb.Response{
+		Message: fmt.Sprintf("Hello, %s!", name),
+		Headers: fmt.Sprintf("%s", md),
+	}
+	return response, nil
+}
